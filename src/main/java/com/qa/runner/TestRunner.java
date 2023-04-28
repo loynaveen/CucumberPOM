0c073b40d6c30630c33548cbe59dbb36d97291b7
Loyola Dsouza <loyoladsouza@loyolas-mbp.home>
2023-04-28 11:52:35 -0400

    minor changes

--- src/main/java/com/qa/runner/TestRunner.java
+++ src/main/java/com/qa/runner/TestRunner.java
@@ -10,9 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 		"/Users/loyoladsouza/Desktop/QA/SeleniumWorkspace/CucumberPOM/src/main/java/com/qa/features/freecrm.feature" }, glue = {
 				"com/qa/stepDefinitions" }, plugin = { "pretty", "html:html-output/cucumber.html",
 						"json:json_output/cucumber.json",
-						"junit:junit_xml/cucumber.xml" }, dryRun = false, monochrome = true
-// tags = "@SmokeTest or @RegressionTest or @End2End"
-)
+						"junit:junit_xml/cucumber.xml" }, dryRun = false, monochrome = true)
 
 public class TestRunner {
 
